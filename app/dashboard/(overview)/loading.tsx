b97f25687a377f3dbed9cd03a136830cd93d660a
Andre Luis Haeberlin <andreh@weg.net>
2025-02-26 16:55:59 -0300

    Chapter 12

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
+
+//first loading function
+//export default function Loading() {
+//    return <div>Loading...</div>;
+//  }
